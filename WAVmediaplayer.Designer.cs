4a8c5c0b2938340c68d6615a5d8c5c7701ea0f83
user <user@1008-021>
2026-05-06 08:34:33 +0800

    加入專案檔案。

---
+++ WAVmediaplayer.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace WAVmediapaly
+{
+    partial class WAVmediaplayer
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.location = new System.Windows.Forms.GroupBox();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.ofdWAVFile = new System.Windows.Forms.OpenFileDialog();
+            this.tbProgress = new System.Windows.Forms.TrackBar();
+            this.lblTotalTime = new System.Windows.Forms.Label();
+            this.timerProgress = new System.Windows.Forms.Timer(this.components);
+            this.tbVolume = new System.Windows.Forms.TrackBar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btn_Play = new System.Windows.Forms.PictureBox();
+            this.btn_Replay = new System.Windows.Forms.PictureBox();
+            this.btn_quit = new System.Windows.Forms.PictureBox();
+            this.btn_Stop = new System.Windows.Forms.PictureBox();
+            this.lblCurrentTime = new System.Windows.Forms.Label();
+            this.location.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tbProgress)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbVolume)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_Play)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_Replay)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_quit)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_Stop)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // location
+            // 
+            this.location.Controls.Add(this.txtPath);
+            this.location.Controls.Add(this.btnBrowse);
+            this.location.Location = new System.Drawing.Point(53, 12);
+            this.location.Name = "location";
+            this.location.Size = new System.Drawing.Size(598, 100);
+            this.location.TabIndex = 0;
+            this.location.TabStop = false;
+            this.location.Text = "音效位置";
+            this.location.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Font = new System.Drawing.Font("新細明體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtPath.Location = new System.Drawing.Point(21, 33);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(387, 40);
+            this.txtPath.TabIndex = 0;
+            this.txtPath.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnBrowse.Location = new System.Drawing.Point(444, 37);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(126, 36);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "瀏覽檔案";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // ofdWAVFile
+            // 
+            this.ofdWAVFile.FileName = "ofdWAVFile";
+            // 
+            // tbProgress
+            // 
+            this.tbProgress.Location = new System.Drawing.Point(108, 118);
+            this.tbProgress.Name = "tbProgress";
+            this.tbProgress.Size = new System.Drawing.Size(494, 45);
+            this.tbProgress.TabIndex = 5;
+            this.tbProgress.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // lblTotalTime
+            // 
+            this.lblTotalTime.AutoSize = true;
+            this.lblTotalTime.Location = new System.Drawing.Point(556, 166);
+            this.lblTotalTime.Name = "lblTotalTime";
+            this.lblTotalTime.Size = new System.Drawing.Size(33, 12);
+            this.lblTotalTime.TabIndex = 6;
+            this.lblTotalTime.Text = "label1";
+            this.lblTotalTime.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // timerProgress
+            // 
+            this.timerProgress.Tick += new System.EventHandler(this.timerProgress_Tick);
+            // 
+            // tbVolume
+            // 
+            this.tbVolume.Location = new System.Drawing.Point(539, 212);
+            this.tbVolume.Maximum = 100;
+            this.tbVolume.Name = "tbVolume";
+            this.tbVolume.Size = new System.Drawing.Size(112, 45);
+            this.tbVolume.TabIndex = 7;
+            this.tbVolume.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.tbVolume.Value = 50;
+            this.tbVolume.Scroll += new System.EventHandler(this.tbVolume_Scroll);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WAVmediapaly.Properties.Resources.sound;
+            this.pictureBox1.Location = new System.Drawing.Point(497, 212);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(47, 29);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btn_Play
+            // 
+            this.btn_Play.Image = global::WAVmediapaly.Properties.Resources.play;
+            this.btn_Play.Location = new System.Drawing.Point(285, 201);
+            this.btn_Play.Name = "btn_Play";
+            this.btn_Play.Size = new System.Drawing.Size(46, 40);
+            this.btn_Play.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btn_Play.TabIndex = 3;
+            this.btn_Play.TabStop = false;
+            this.btn_Play.Click += new System.EventHandler(this.btn_Play_Click);
+            // 
+            // btn_Replay
+            // 
+            this.btn_Replay.Image = global::WAVmediapaly.Properties.Resources.replay;
+            this.btn_Replay.Location = new System.Drawing.Point(194, 201);
+            this.btn_Replay.Name = "btn_Replay";
+            this.btn_Replay.Size = new System.Drawing.Size(47, 40);
+            this.btn_Replay.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btn_Replay.TabIndex = 4;
+            this.btn_Replay.TabStop = false;
+            this.btn_Replay.Click += new System.EventHandler(this.btn_Replay_Click);
+            // 
+            // btn_quit
+            // 
+            this.btn_quit.Image = global::WAVmediapaly.Properties.Resources.close;
+            this.btn_quit.Location = new System.Drawing.Point(384, 201);
+            this.btn_quit.Name = "btn_quit";
+            this.btn_quit.Size = new System.Drawing.Size(46, 40);
+            this.btn_quit.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btn_quit.TabIndex = 2;
+            this.btn_quit.TabStop = false;
+            this.btn_quit.Click += new System.EventHandler(this.btn_quit_Click);
+            // 
+            // btn_Stop
+            // 
+            this.btn_Stop.Image = global::WAVmediapaly.Properties.Resources.stop;
+            this.btn_Stop.Location = new System.Drawing.Point(285, 201);
+            this.btn_Stop.Name = "btn_Stop";
+            this.btn_Stop.Size = new System.Drawing.Size(46, 40);
+            this.btn_Stop.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btn_Stop.TabIndex = 1;
+            this.btn_Stop.TabStop = false;
+            this.btn_Stop.Click += new System.EventHandler(this.btn_Stop_Click);
+            // 
+            // lblCurrentTime
+            // 
+            this.lblCurrentTime.AutoSize = true;
+            this.lblCurrentTime.Location = new System.Drawing.Point(106, 166);
+            this.lblCurrentTime.Name = "lblCurrentTime";
+            this.lblCurrentTime.Size = new System.Drawing.Size(32, 12);
+            this.lblCurrentTime.TabIndex = 9;
+            this.lblCurrentTime.Text = "00:00";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(725, 253);
+            this.Controls.Add(this.lblCurrentTime);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.tbVolume);
+            this.Controls.Add(this.lblTotalTime);
+            this.Controls.Add(this.tbProgress);
+            this.Controls.Add(this.btn_Play);
+            this.Controls.Add(this.btn_Replay);
+            this.Controls.Add(this.location);
+            this.Controls.Add(this.btn_quit);
+            this.Controls.Add(this.btn_Stop);
+            this.Name = "Form1";
+            this.Text = "WAV媒體播放器";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.location.ResumeLayout(false);
+            this.location.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tbProgress)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbVolume)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_Play)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_Replay)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_quit)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_Stop)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox location;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.OpenFileDialog ofdWAVFile;
+        private System.Windows.Forms.PictureBox btn_Stop;
+        private System.Windows.Forms.PictureBox btn_quit;
+        private System.Windows.Forms.PictureBox btn_Replay;
+        private System.Windows.Forms.PictureBox btn_Play;
+        private System.Windows.Forms.TrackBar tbProgress;
+        private System.Windows.Forms.Label lblTotalTime;
+        private System.Windows.Forms.Timer timerProgress;
+        private System.Windows.Forms.TrackBar tbVolume;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblCurrentTime;
+    }
+}
+
---
+++ WAVmediaplayer.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio.Wave;
+
+
+
+namespace WAVmediapaly
+{
+    
+    public partial class WAVmediaplayer : Form
+    {
+        private AudioFileReader audioFile;
+        private WaveOutEvent outputDevice;
+        private bool isLooping = false; // 紀錄是否要重複播放
+
+
+
+        public WAVmediaplayer()
+        {
+            InitializeComponent();
+            btn_Play.Enabled = false;       // 播放按鈕停用
+            btn_Replay.Enabled = false;       // 重複播放停用
+            btn_Stop.Enabled = false;       // 停止按鈕停用
+        }
+
+        // --- 瀏覽檔案 ---
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            ofdWAVFile.Filter = "WAV Files(*.wav)|*.wav";
+            if (ofdWAVFile.ShowDialog() == DialogResult.OK)
+            {
+                txtPath.Text = ofdWAVFile.FileName;
+
+                // 選新檔案時，先把舊的資源釋放，避免佔用
+                DisposePlayback();
+
+                btn_Play.Enabled = true;
+                btn_Replay.Enabled = true;
+
+                // 重置進度條
+                tbProgress.Value = 0;
+                lblCurrentTime.Text = "00:00";
+            }
+        }
+
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    
+
+        // --- 播放 / 繼續 ---
+        private void btn_Play_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtPath.Text)) return;
+
+            try
+            {
+                if (outputDevice == null)
+                {
+                    outputDevice = new WaveOutEvent();
+                    audioFile = new AudioFileReader(txtPath.Text);
+                    outputDevice.Init(audioFile);
+                    outputDevice.PlaybackStopped += OnPlaybackStopped;
+
+                    // 重要：設定進度條的最大值為音樂總秒數
+                    tbProgress.Maximum = (int)audioFile.TotalTime.TotalSeconds;
+                    lblTotalTime.Text = audioFile.TotalTime.ToString(@"mm\:ss"); // 假設你有這個 Label
+                }
+
+                outputDevice.Play();
+                timerProgress.Start();
+
+                // UI 切換
+                TogglePlayButtons(true);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("播放出錯：" + ex.Message);
+            }
+        }
+
+        // --- 暫停 (你的暫停鍵叫做 btn_Stop) ---
+        private void btn_Stop_Click(object sender, EventArgs e)
+        {
+            if (outputDevice != null && outputDevice.PlaybackState == PlaybackState.Playing)
+            {
+                outputDevice.Pause();
+                timerProgress.Stop();
+                TogglePlayButtons(false);
+            }
+        }
+
+        // --- 重複播放 (切換開關) ---
+        private void btn_Replay_Click(object sender, EventArgs e)
+        {
+            isLooping = !isLooping;
+
+            if (isLooping)
+            {
+                btn_Replay.BackColor = Color.LightSkyBlue; // 變色提示開啟
+                // MessageBox.Show("已開啟重複播放");
+            }
+            else
+            {
+                btn_Replay.BackColor = SystemColors.Control; // 恢復原色
+            }
+        }
+
+        // --- 播放結束事件 (最重要：解決報錯的地方) ---
+        private void OnPlaybackStopped(object sender, StoppedEventArgs e)
+        {
+            // 使用 Invoke 確保回到 UI 執行緒執行
+            this.Invoke(new Action(() =>
+            {
+                if (isLooping && audioFile != null && outputDevice != null)
+                {
+                    // 重複播放邏輯
+                    audioFile.Position = 0;
+                    outputDevice.Play();
+                    tbProgress.Value = 0;
+                }
+                else
+                {
+                    // 正常停止
+                    timerProgress.Stop();
+                    TogglePlayButtons(false);
+
+                    // 如果播完了，把進度歸零
+                    if (audioFile != null && audioFile.Position >= audioFile.Length)
+                    {
+                        audioFile.Position = 0;
+                        tbProgress.Value = 0;
+                        lblCurrentTime.Text = "00:00";
+                    }
+                }
+            }));
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    
+
+        private void ofdWAVFile_HelpRequest(object sender, EventArgs e)
+        {
+            // 使用完整檔名建立物件
+            SoundPlayer player2 = new SoundPlayer(txtPath.Text);
+            player2.PlayLooping();
+        }
+
+
+
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            if (audioFile != null)
+            {
+                // 當使用者拖曳滑桿時，把音樂目前的播放時間，改為滑桿所在的秒數
+                audioFile.CurrentTime = TimeSpan.FromSeconds(tbProgress.Value);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // 為了避免手動拖曳時跟 Timer 互相干擾，設定一個變數
+        private bool isDraggingProgress = false;
+
+       
+
+        // --- 音量控制 ---
+        private void tbVolume_Scroll(object sender, EventArgs e)
+        {
+            if (audioFile != null)
+            {
+                audioFile.Volume = tbVolume.Value / 100f;
+            }
+        }
+
+
+         private void timerProgress_Tick(object sender, EventArgs e)
+        {
+            if (audioFile != null && !isDraggingProgress)
+            {
+                // 確保數值不超過最大值
+                int currentSec = (int)audioFile.CurrentTime.TotalSeconds;
+                tbProgress.Value = Math.Min(currentSec, tbProgress.Maximum);
+                lblCurrentTime.Text = audioFile.CurrentTime.ToString(@"mm\:ss");
+            }
+        }
+        private void tbProgress_MouseDown(object sender, MouseEventArgs e) { isDraggingProgress = true; }
+        private void tbProgress_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (audioFile != null)
+            {
+                audioFile.CurrentTime = TimeSpan.FromSeconds(tbProgress.Value);
+            }
+            isDraggingProgress = false;
+        }
+        private void tbProgress_Scroll(object sender, EventArgs e)
+        {
+            TimeSpan dragTime = TimeSpan.FromSeconds(tbProgress.Value);
+            lblCurrentTime.Text = dragTime.ToString(@"mm\:ss");
+        }
+
+        // --- 工具方法：切換按鈕顯示 ---
+        private void TogglePlayButtons(bool playing)
+        {
+            btn_Play.Visible = !playing;
+            btn_Play.Enabled = !playing;
+            btn_Stop.Visible = playing;
+            btn_Stop.Enabled = playing;
+        }
+
+        // --- 釋放資源 ---
+        private void DisposePlayback()
+        {
+            outputDevice?.Stop();
+            outputDevice?.Dispose();
+            outputDevice = null;
+            audioFile?.Dispose();
+            audioFile = null;
+        }
+
+        private void btn_quit_Click(object sender, EventArgs e) { Application.Exit(); }
+
+        private void frmWAVPlayer_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("確定要關閉嗎？", "確認", MessageBoxButtons.YesNo) == DialogResult.No)
+                e.Cancel = true;
+            else
+                DisposePlayback();
+        }
+    }
+}
